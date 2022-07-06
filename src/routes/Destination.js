35b796601821451593eaca3da2f205ebc3eda1c4
francesm <fr.ncesm@gmail.com>
2022-07-06 16:10:41 -0600

    add crew page and initial styling

--- src/routes/Destination.js
+++ src/routes/Destination.js
@@ -8,8 +8,8 @@ import marsImg from '../assets/destination/image-mars.webp';
 import europaImg from '../assets/destination/image-europa.webp';
 import titanImg from '../assets/destination/image-titan.webp';
 
-export const Destination = () => {
-    const [destination, setDestination] = useState({
+const destinations = {
+    moon: {
         name: "moon",
         description: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
         regain perspective and come back refreshed. While you’re there, take in some history
@@ -18,51 +18,43 @@ export const Destination = () => {
         travelTime: "3 days",
         imageSrc: moonImg,
         alt: "Earth's moon"
-    });
-
-    const destinations = {
-        moon: {
-            name: "moon",
-            description: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
-            regain perspective and come back refreshed. While you’re there, take in some history
-            by visiting the Luna 2 and Apollo 11 landing sites.`,
-            distance: "384,400 km",
-            travelTime: "3 days",
-            imageSrc: moonImg,
-            alt: "Earth's moon"
-        },
-        mars: {
-            name: "mars",
-            description: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, 
-            the tallest planetary mountain in our solar system. It’s two and a half times 
-            the size of Everest!`,
-            distance: "225 mil. km",
-            travelTime: "9 months",
-            imageSrc: marsImg,
-            alt: "the planet Mars"
-        },
-        europa: {
-            name: "europa",
-            description: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a 
-            winter lover’s dream. With an icy surface, it’s perfect for a bit of 
-            ice skating, curling, hockey, or simple relaxation in your snug 
-            wintery cabin.`,
-            distance: "628 mil. km",
-            travelTime: "3 years",
-            imageSrc: europaImg,
-            alt: "Europa, one of Jupiter's moons"
-        },
-        titan: {
-            name: "titan",
-            description: `The only moon known to have a dense atmosphere other than Earth, Titan 
-            is a home away from home (just a few hundred degrees colder!). As a 
-            bonus, you get striking views of the Rings of Saturn.`,
-            distance: "1.6 bil. km",
-            travelTime: "7 years",
-            imageSrc: titanImg,
-            alt: "Titan, one of Saturn's moons"
-        }
+    },
+    mars: {
+        name: "mars",
+        description: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, 
+        the tallest planetary mountain in our solar system. It’s two and a half times 
+        the size of Everest!`,
+        distance: "225 mil. km",
+        travelTime: "9 months",
+        imageSrc: marsImg,
+        alt: "the planet Mars"
+    },
+    europa: {
+        name: "europa",
+        description: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a 
+        winter lover’s dream. With an icy surface, it’s perfect for a bit of 
+        ice skating, curling, hockey, or simple relaxation in your snug 
+        wintery cabin.`,
+        distance: "628 mil. km",
+        travelTime: "3 years",
+        imageSrc: europaImg,
+        alt: "Europa, one of Jupiter's moons"
+    },
+    titan: {
+        name: "titan",
+        description: `The only moon known to have a dense atmosphere other than Earth, Titan 
+        is a home away from home (just a few hundred degrees colder!). As a 
+        bonus, you get striking views of the Rings of Saturn.`,
+        distance: "1.6 bil. km",
+        travelTime: "7 years",
+        imageSrc: titanImg,
+        alt: "Titan, one of Saturn's moons"
     }
+}
+
+export const Destination = () => {
+    const [destination, setDestination] = useState(destinations.moon);
+
 
     const handleDestinationClick = ({ target }) => {
         const name = target.value;
@@ -85,10 +77,10 @@ export const Destination = () => {
                         <div className="destination__text-container">
                             <nav className="destination__nav">
                                 <ul>
-                                    <li><button className={destination.name === "moon" ? "nav-btn active" : "nav-btn"} value="moon" onClick={handleDestinationClick}>Moon</button></li>
-                                    <li><button className={destination.name === "mars" ? "nav-btn active" : "nav-btn"} value="mars" onClick={handleDestinationClick}>Mars</button></li>
-                                    <li><button className={destination.name === "europa" ? "nav-btn active" : "nav-btn"} value="europa" onClick={handleDestinationClick}>Europa</button></li>
-                                    <li><button className={destination.name === "titan" ? "nav-btn active" : "nav-btn"} value="titan" onClick={handleDestinationClick}>Titan</button></li>
+                                    <li><button className={destination.name === "moon" ? "nav-btn active-tab" : "nav-btn"} value="moon" onClick={handleDestinationClick}>Moon</button></li>
+                                    <li><button className={destination.name === "mars" ? "nav-btn active-tab" : "nav-btn"} value="mars" onClick={handleDestinationClick}>Mars</button></li>
+                                    <li><button className={destination.name === "europa" ? "nav-btn active-tab" : "nav-btn"} value="europa" onClick={handleDestinationClick}>Europa</button></li>
+                                    <li><button className={destination.name === "titan" ? "nav-btn active-tab" : "nav-btn"} value="titan" onClick={handleDestinationClick}>Titan</button></li>
                                 </ul>
                             </nav>
                             <h2 className="destination__name">{destination.name}</h2>
